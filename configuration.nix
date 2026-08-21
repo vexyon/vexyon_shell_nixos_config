e57222aa105b7358543b0232c4643a2e0731fa0b
vexyon <vexyon@proton.me>
2026-08-21 15:54:54 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -165,6 +165,15 @@
 
   #Bluetooth
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  boot.extraModprobeConfig = ''
+    options ideapad_laptop no_bt_rfkill=1
+  '';
+
+  systemd.tmpfiles.rules = [
+    "r /var/lib/systemd/rfkill/pci-0000:00:14.0-usb-0:14:1.0:bluetooth"
+  ];
   #Tailscale
   services.tailscale.enable = true;
 
