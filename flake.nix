ad1878855ec1d3079637077335459c25a0f75f45
vexyon <vexyon@proton.me>
2026-09-06 21:09:13 +0200

    Add hermes-agent to flake.nix inputs and packages

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     vexyon.url = "github:vexyon/vexyon_shell_nixos";
     comfyui-nix.url = "github:utensils/comfyui-nix";
+    hermes-agent.url = "github:NousResearch/hermes-agent";
   };
 
-  outputs = inputs@{ nixpkgs, vexyon, comfyui-nix, ... }:
+  outputs = inputs@{ nixpkgs, vexyon, comfyui-nix, hermes-agent, ... }:
     let
       system = "x86_64-linux";
     in
@@ -23,6 +24,10 @@
             };
 
             users.users.vexyon.extraGroups = [ "wheel" ];
+            environment.systemPackages = [
+              hermes-agent.packages.${system}.minimal.hermesDesktop
+
+            ];
           }
         ];
       };
